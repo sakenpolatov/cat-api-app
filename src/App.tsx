8441de103e41a898cfae7918e75fe5201602d4e6
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-05-02 11:38:36 +0500

    feat : add app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { Container, Controls, ImageContainer } from "./components";
+import { useGetCatImage } from "./hooks/getCatImage";
+
 function App() {
-  return <div className="App"></div>;
+  const [enabled, setEnabled] = useState(true);
+  const [autoRefresh, setAutoRefresh] = useState(false);
+
+  const { catUrl, loading, fetchCatImage } = useGetCatImage(
+    enabled,
+    autoRefresh
+  );
+
+  return (
+    <Container>
+      <Controls
+        enabled={enabled}
+        autoRefresh={autoRefresh}
+        loading={loading}
+        onToggleEnabled={() => setEnabled((prev) => !prev)}
+        onToggleAutoRefresh={() => setAutoRefresh((prev) => !prev)}
+        onGetCat={fetchCatImage}
+      />
+
+      <ImageContainer src={catUrl} loading={loading} />
+    </Container>
+  );
 }
 
 export default App;
